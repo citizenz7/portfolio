26a37d764c1888ed50e8024766139c18c41bc3e2
pengolincoin <pengolincoin@s2ii.xyz>
2020-07-18 11:55:30 +0200

    commentaires Disqus articles + projets

--- article.php
+++ article.php
@@ -22,8 +22,29 @@ if($row['articleID'] == ''){
           </p>
         </div>
         <img class="img-fluid img-thumbnail float-left img-article" src="<?php echo $row['articleImage']; ?>" alt="<?php html($row['articleTitre']); ?>">
-          <?php echo $row['articleTexte']; ?>
-      </div>
+	  <?php echo $row['articleTexte']; ?>
+
+	<div id="disqus_thread" class="pt-5"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://portfolio-shh9xt0mnz.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	</div>
     </div>
   </div>
 
